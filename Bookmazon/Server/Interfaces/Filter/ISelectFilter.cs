6908b0d263ef2edb20ede2d8c9d2f2d99a897115
Niklas Rettenwander <61598102+rettenwander@users.noreply.github.com>
2021-12-09 15:53:19 +0100

    wip (#4)
    Co-authored-by: Niklas Retenwander <niklas.retti@gmail.com>

---
+++ Bookmazon/Server/Interfaces/Filter/ISelectFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookmazon.Server.Interfaces.Filter
+{
+    public interface ISelectFilter<T>
+    {
+        ICollection<T> FilterList { get; init; }
+
+        public bool InList(T value);
+    }
+}
---
+++ Bookmazon/Server/Interfaces/Repos/IBookRepo.cs
@@ -0,0 +1,61 @@
+﻿using Bookmazon.Server.Interfaces.Filter;
+using Bookmazon.Shared.Models;
+
+namespace Bookmazon.Server.Interfaces.Repos
+{
+    public interface IBookRepo
+    {
+        #region Book
+        Task<Book?> GetBook(string ISBN);
+        Task<IEnumerable<Book>> GetAllBooks(IBookFilter? bookFilter);
+        Task<IEnumerable<Book>> GetAllBooksWhere(Func<Book, bool> where);
+        Task<IEnumerable<Book>> SearchBookByTitle(string bookTitle, Func<Book, bool> where);
+        void AddBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(Book book);
+
+        #endregion
+
+
+        #region VAT
+        Task<VAT?> GetVAT(int VATID);
+        Task<ICollection<VAT>> GetAllVATs();
+        void AddVAT(VAT vat);
+        void UpdateVAT(VAT vat);
+        void DeleteVAT(VAT VAT);
+
+        #endregion
+
+        #region language
+        Task<Language?> GetLanguage(string languageCode);
+        Task<IEnumerable<Language>> GetAllLanguages();
+        void AddLanguage(Language language);
+        void UpdateLanguage(Language language);
+        void DeleteLanguage(Language language);
+        #endregion
+
+        #region Ganre
+        Task<Genre?> GetGenre(int genreId);
+        Task<IEnumerable<Genre>> GetAllGenre();
+        void AddGenre(Genre genre);
+        void UpdateGenre(Genre genre);
+        void DeleteGenre(Genre genre);
+        #endregion
+
+        #region Author
+        Task<Author?> GetAuthor(int authorId);
+        Task<IEnumerable<Author>> GetAllAuthor();
+        void AddAuthor(Author author);
+        void UpdateAuthor(Author author);
+        void DeleteAuthor(Author author);
+        #endregion
+
+        #region Publisher
+        Task<Publisher?> GetPublisher(int publisherId);
+        Task<IEnumerable<Publisher>> GetAllPublisher();
+        void AddPublisher(Publisher publisher);
+        void UpdatePublisher(Publisher publisher);
+        void DeletePublisher(Publisher publisher);
+        #endregion
+    }
+}
