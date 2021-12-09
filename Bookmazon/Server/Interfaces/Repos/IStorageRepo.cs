45d157476f59bb50890aca717cf5247a8f3af22a
Hari <haribobie@gmx.at>
2021-12-09 15:48:53 +0100

    Add interfaces for repos
    Added Interfaces for following Repos
    - CustomerOrderRepo
    - StorageRepo
    - SupplierRepo
    - SupplyOrderRepo
    - UserRepo

---
+++ Bookmazon/Server/Interfaces/Repos/IStorageRepo.cs
@@ -0,0 +1,29 @@
+﻿using Bookmazon.Shared.Models;
+
+namespace Bookmazon.Server.Interfaces.Repos
+{
+    public interface IStorageRepo
+    {
+        #region Storage
+        // Get
+        Task<IEnumerable<Storage>> GetAllStorages();
+        Task<Storage?> GetStorage(string storageId);
+        
+        // Set
+        void AddStorage(Storage storage);
+        void UpdateStorage(Storage storage);
+        void RemoveStorage(Storage storage);
+        #endregion
+
+        #region StorageLocation
+        // Get
+        Task<IEnumerable<StorageLocation>> GetAllStorageLocations();
+        Task<StorageLocation> GetStorageLocation(string storageId);
+
+        // Set
+        void AddStorageLocation(StorageLocation storageLocation);
+        void UpdateStorageLocation(StorageLocation storageLocation);
+        void RemoveStorageLocation(StorageLocation storageLocation);
+        #endregion
+    }
+}
