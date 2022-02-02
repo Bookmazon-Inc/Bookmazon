8da1b54fefd4429f26590e6496f45a4ab8aae730
Hari <haribobie@gmx.at>
2022-02-02 22:37:54 +0100

    CustomerOrder (ToTest)
    Has to be tested (couldnt test bc Error:
    - Exception : IDX10223: Lifetime validation failed. The token is expired. ValidTo: 'System.DateTime', Current time: 'System.DateTime'.)

---
+++ Bookmazon/Server/Controllers/CustomerOrderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookmazon.Server.Data;
+using Bookmazon.Shared.Dtos.Cart;
+using Bookmazon.Shared.Models;
+
+namespace Bookmazon.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerOrderController : Controller
+    {
+        // Privates
+        private UnitOfWork _uow;
+
+        public CustomerOrderController(UnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="customerOrder"></param>
+        [HttpPost("CreateOrder")]
+        public async void CreateOrder(CustomerOrder ord)
+        {
+            _uow.customerOrderRepo.AddCustomerOrder(ord);
+        }
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Returns all CustomerOrders in the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerOrder>>> Index()
+        {
+            var cuo = await _uow.customerOrderRepo.GetAllCustomerOrders(null);
+            return Ok(cuo);
+        }
+    }
+}
