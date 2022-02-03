cf7831b6280014bb415f830b4205a5e8e509f872
Hari <haribobie@gmx.at>
2022-02-03 15:20:00 +0100

    final

---
+++ Bookmazon/Server/Migrations/20220203141146_testneu.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookmazon.Server.Migrations
+{
+    public partial class testneu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                schema: "ord",
+                table: "SupplyOrder");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Discount",
+                schema: "ord",
+                table: "SupplyOrder",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
